8b398d5e179d8acc11f4b12b13cfbd636ab95ec2
+ <+>
2024-01-10 12:51:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // convert the word to lower Case
+  const cleanWord = word.toLowerCase()
+
+  // create a reversed word
+  const reversedWord = cleanWord.split('').reverse().join('');
+  
+  // return true if the the owrd is a paliondrome otherwise return false
+  return cleanWord === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  1.Change the word to lowercase
+  2.reverse the word
+  3.compare the reversed word to the original word
+  4.If they are similar return true otherwise false
 */
 
 /*
   Add written explanation of your solution here
+  the function first cleans the input word by changing it to lowercase.
+  then the input word is reversed.
+  after that the input word is compared to the reversed word.
+  then if the two words match return true otherwise return false 
 */
 
 // You can run `node index.js` to view these console logs
